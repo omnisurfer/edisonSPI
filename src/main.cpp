1e4184361adfa69fd35bd97ea3acff328a2ebdc4
Daniel <omnisurfer@gmail.com>
2020-06-20 14:03:21 -0700

    copied demo code over, seems to be working! port is likely good to use.

--- src/main.cpp
+++ src/main.cpp
@@ -68,6 +68,7 @@ int main() {
         EPD_4IN2_Clear();
         DEV_Delay_ms(500);
 
+#if 1
         //Create a new image cache
         UBYTE *BlackImage;
         /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
@@ -76,8 +77,49 @@ int main() {
             printf("Failed to apply for black memory...\r\n");
             return -1;
         }
-        //printf("Paint_NewImage\r\n");
-        //Paint_NewImage(BlackImage, EPD_4IN2_WIDTH, EPD_4IN2_HEIGHT, 0, WHITE);
+        printf("Paint_NewImage\r\n");
+        Paint_NewImage(BlackImage, EPD_4IN2_WIDTH, EPD_4IN2_HEIGHT, 0, WHITE);
+
+        EPD_4IN2_Init_4Gray();
+		printf("show Gray------------------------\r\n");
+		free(BlackImage);
+		BlackImage = NULL;
+		Imagesize = ((EPD_4IN2_WIDTH % 8 == 0)? (EPD_4IN2_WIDTH / 4 ): (EPD_4IN2_WIDTH / 4 + 1)) * EPD_4IN2_HEIGHT;
+		if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
+			printf("Failed to apply for black memory...\r\n");
+			return -1;
+		}
+		Paint_NewImage(BlackImage, EPD_4IN2_WIDTH, EPD_4IN2_HEIGHT, 0, WHITE);
+		Paint_SetScale(4);
+		Paint_Clear(WHITE);
+
+		Paint_DrawPoint(10, 80, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
+	    Paint_DrawPoint(10, 90, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
+	    Paint_DrawPoint(10, 100, BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
+	    Paint_DrawLine(20, 70, 70, 120, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+	    Paint_DrawLine(70, 70, 20, 120, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+	    Paint_DrawRectangle(20, 70, 70, 120, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+	    Paint_DrawRectangle(80, 70, 130, 120, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+	    Paint_DrawCircle(45, 95, 20, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+	    Paint_DrawCircle(105, 95, 20, WHITE, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+	    Paint_DrawLine(85, 95, 125, 95, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+	    Paint_DrawLine(105, 75, 105, 115, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+	    Paint_DrawString_EN(10, 0, "waveshare", &Font16, BLACK, WHITE);
+	    Paint_DrawString_EN(10, 20, "hello world", &Font12, WHITE, BLACK);
+	    Paint_DrawNum(10, 33, 123456789, &Font12, BLACK, WHITE);
+	    Paint_DrawNum(10, 50, 987654321, &Font16, WHITE, BLACK);
+	    Paint_DrawString_CN(140, 0, "���abc", &Font12CN, GRAY1, GRAY4);
+	    Paint_DrawString_CN(140, 40, "���abc", &Font12CN, GRAY2, GRAY3);
+	    Paint_DrawString_CN(140, 80, "���abc", &Font12CN, GRAY3, GRAY2);
+	    Paint_DrawString_CN(140, 120, "���abc", &Font12CN, GRAY4, GRAY1);
+
+	    Paint_DrawString_CN(220, 0, "΢ѩ����", &Font24CN, GRAY1, GRAY4);
+	    Paint_DrawString_CN(220, 40, "΢ѩ����", &Font24CN, GRAY2, GRAY3);
+	    Paint_DrawString_CN(220, 80, "΢ѩ����", &Font24CN, GRAY3, GRAY2);
+	    Paint_DrawString_CN(220, 120, "΢ѩ����", &Font24CN, GRAY4, GRAY1);
+
+		EPD_4IN2_4GrayDisplay(BlackImage);
+		DEV_Delay_ms(2000);
 #endif
 
 	return 0;
